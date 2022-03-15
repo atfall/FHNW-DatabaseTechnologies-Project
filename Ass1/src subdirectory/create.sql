5940bd2909839018f8e4db91d7c4ef3540aa226d
romano <>
2022-03-15 22:00:44 +0100

    added insurance_coverage create table statement

--- Ass1/src subdirectory/create.sql
+++ Ass1/src subdirectory/create.sql
@@ -1,8 +1,17 @@
 ---- Create table insurance_company
-CREATE TABLE Customer
+CREATE TABLE insurance_company
 (
 icompany_id         INT             NOT NULL, 
-company_name        VARCHAR(30)     NOT NULL,
-company_adress      VARCHAR(50)     NOT NULL,
-package_ID          INT             NOT NULL,
+icompany_name        VARCHAR(30)     NOT NULL,
+icompany_adress      VARCHAR(50)     NOT NULL,
 PRIMARY KEY   (icompany_id));
+
+
+---- Create table insurance_coverage
+CREATE TABLE insurance_coverage
+(
+ipackage_id         INT             NOT NULL, 
+ipackage_name       VARCHAR(30)     NOT NULL,
+ipackage_cost       INT             NOT NULL,
+icompany_id         INT             NOT NULL,
+PRIMARY KEY   (ipackage_id));
