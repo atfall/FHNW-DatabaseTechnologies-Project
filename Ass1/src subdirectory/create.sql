91f06f1b23c5b46e8e2e1822b2258debac7b4ca4
romano <>
2022-03-15 21:43:56 +0100

    added insurance_company create table statement

---
+++ Ass1/src subdirectory/create.sql
@@ -0,0 +1,8 @@
+---- Create table insurance_company
+CREATE TABLE Customer
+(
+icompany_id         INT             NOT NULL, 
+company_name        VARCHAR(30)     NOT NULL,
+company_adress      VARCHAR(50)     NOT NULL,
+package_ID          INT             NOT NULL,
+PRIMARY KEY   (icompany_id));
