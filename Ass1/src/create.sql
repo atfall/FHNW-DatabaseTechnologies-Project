297ab33828d0dfc2421d14a8050bcb4a855294dc
Adam <adam.threlfall@students.fhnw.ch>
2022-03-22 19:14:40 +0100

    Added data and formatting
    Inserted more data in to tables for customer and car in create.sql,
    Formatting for create.sql,
    Formatting for queries1.sql

--- Ass1/src/create.sql
+++ Ass1/src/create.sql
@@ -46,31 +46,22 @@ FOREIGN KEY (customer_id) REFERENCES customer(customer_id));
 ---- Fill table insurance_company
 INSERT INTO insurance_company VALUES
 (1, 'Ooops Insurance', 'Next Corner Left 46, 4001 Basel');
-
 INSERT INTO insurance_company VALUES
 (2, 'I did it again Insurance Inc.', 'Somewhere over trb, 6060 Overthere');
-
 INSERT INTO insurance_company VALUES
 (3, 'Hit me Insure. and Co', 'Wordstreet 11, 4002 Basel');
-
 INSERT INTO insurance_company VALUES
 (4, 'Crash test dummies Inc.', 'Death Star 1, 666 Orbit');
-
 INSERT INTO insurance_company VALUES
 (5, 'How Bizarre Versicherungen', 'Sm�rebr�d, 898989 Upsala');
-
 INSERT INTO insurance_company VALUES
 (6, 'Dont look back in Anger', 'Knie Circus 1, 8001 Z�ri');
-
 INSERT INTO insurance_company VALUES
 (7, 'Crash test dummies Inc.', 'Whats the story 1th, 9 London');
-
 INSERT INTO insurance_company VALUES
 (8, 'Would I lie to you?', 'Spotify 111, 401 HTTP');
-
 INSERT INTO insurance_company VALUES
 (9, 'Joyride', 'Sm�rebr�d, 898989 Upsala');
-
 INSERT INTO insurance_company VALUES
 (10, 'Shiny Happy People (SHP)', 'Hotel California, 1-2-1 Dessert');
 
@@ -90,9 +81,41 @@ INSERT INTO customer VALUES
 (2001, 'John', 'Addams', 'Home Town', 101);
 INSERT INTO customer VALUES
 (2002, 'Mary', 'Jones', 'New Town', 102);
+INSERT INTO customer VALUES
+(2003, 'Stephen', 'Dylan', 'Basel', 103);
+INSERT INTO customer VALUES
+(2004, 'Rebeca', 'Garcia', 'Bern', 104);
+INSERT INTO customer VALUES
+(2005, 'Rudi', 'Beck', 'Basel', 104);
+INSERT INTO customer VALUES
+(2006, 'Louie', 'Quintana', 'Basel', 102);
+INSERT INTO customer VALUES
+(2007, 'Ashanti', 'Ashley', 'Basel', 103);
+INSERT INTO customer VALUES
+(2008, 'Hubert', 'Shelton', 'Bern', 104);
+INSERT INTO customer VALUES
+(2009, 'Tom', 'Piper', 'Basel', 103);
+INSERT INTO customer VALUES
+(2010, 'Jill', 'Morrison', 'Zurich', 104);
 
 ---- Fill table car
 INSERT INTO car VALUES
 (3001, 'Ford', 'Focus', 'Red', 10, 2001);
 INSERT INTO car VALUES
 (3002, 'Tesla', 'Model 3', 'Blue', 20, 2002);
+INSERT INTO car VALUES
+(3003, 'Toyota', 'Prius', 'Blue', 15, 2003);
+INSERT INTO car VALUES
+(3004, 'Volkswagen', 'Golf', 'Black', 25, 2004);
+INSERT INTO car VALUES
+(3005, 'BMW', 'M3', 'Red', 50, 2005);
+INSERT INTO car VALUES
+(3006, 'Mercedes', 'S-Class', 'Silver', 40, 2006);
+INSERT INTO car VALUES
+(3007, 'Volkswagen', 'UP', 'Red', 12, 2007);
+INSERT INTO car VALUES
+(3008, 'BMW', 'i3', 'Black', 15, 2008);
+INSERT INTO car VALUES
+(3009, 'Audi', 'R8', 'Red', 10, 2009);
+INSERT INTO car VALUES
+(3010, 'Volvo', 'V60', 'Blue', 20, 2010);
--- Ass1/src/queries1.sql
+++ Ass1/src/queries1.sql
@@ -1,9 +1,43 @@
 ----One table questions
 --1) Show the car_id, car_brand, car_model and milage of the car that has the lowest milage.
 SELECT car_id, car_brand, car_model, milage FROM car WHERE milage = (SELECT MIN(milage) from car);
+--2)
+
+--3)
+
+--4)
+
+--5)
 
 ----Two table questions
+--6)
+
+--7)
+
+--8)
+
+--9)
+
+--10)
 
 ----Three table questions
+--11)
+
+--12)
+
+--13)
+
+--14)
+
+--15)
 
 ----Four table questions
+--16)
+
+--17)
+
+--18)
+
+--19)
+
+--20)
