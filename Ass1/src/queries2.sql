fc3f5872005a7b0ecd446bffbc57fb852e19e004
Adam <adam.threlfall@students.fhnw.ch>
2022-03-26 09:08:11 +0100

    updated Trigger

--- Ass1/src/queries2.sql
+++ Ass1/src/queries2.sql
@@ -22,10 +22,13 @@ FOR EACH ROW
 DECLARE
 l_transaction VARCHAR2(10);
 BEGIN
--- determine the transaction type l_transaction := CASE
+-- determine the transaction type 
+l_transaction := CASE 
+WHEN UPDATING THEN 'UPDATE'
+WHEN DELETING THEN 'DELETE'
 END;
 -- insert a row into the audit table
-INSERT INTO audits (table_name, transaction_name, by_user, transaction_date) VALUES('CUSTOMERS', l_transaction, USER, SYSDATE);
+INSERT INTO audits (table_name, transaction_name, by_user, transaction_date) VALUES('Car', l_transaction, USER, SYSDATE);
 END;
 
 ----One View of 4 Tables
