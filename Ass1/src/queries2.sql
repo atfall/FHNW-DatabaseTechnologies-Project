162749b79e62f5dee463fe41ceb698043904b176
Adam <adam.threlfall@students.fhnw.ch>
2022-03-26 08:57:48 +0100

    Trigger added

--- Ass1/src/queries2.sql
+++ Ass1/src/queries2.sql
@@ -1,4 +1,5 @@
 ----Stored Procedure
+--Prints car brand and model when the customer id is provided
 CREATE OR REPLACE PROCEDURE carOwned(p_customer_id NUMBER)
 IS
 r_car car%ROWTYPE;
@@ -12,7 +13,20 @@ END;
 /
 
 EXEC carOwned(2001)
+
 ----Trigger
+CREATE OR REPLACE TRIGGER customers_audit_trg AFTER
+UPDATE OR DELETE
+ON "C##MOUSE"."car"
+FOR EACH ROW
+DECLARE
+l_transaction VARCHAR2(10);
+BEGIN
+-- determine the transaction type l_transaction := CASE
+END;
+-- insert a row into the audit table
+INSERT INTO audits (table_name, transaction_name, by_user, transaction_date) VALUES('CUSTOMERS', l_transaction, USER, SYSDATE);
+END;
 
 ----One View of 4 Tables
 DROP VIEW V;
@@ -26,7 +40,9 @@ AND customer.package_id=insurance_coverage.package_id
 AND insurance_coverage.company_id=insurance_company.company_id;
 
 SELECT * FROM V;
+
 ----Update Statement
 UPDATE customer SET customer_city = 'New Bern' WHERE customer_id = 2008;
+
 ----Delete Statement
 DELETE FROM car WHERE car_id = 3011;
