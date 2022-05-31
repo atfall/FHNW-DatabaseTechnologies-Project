9a7381ec497a17193fc162bba2dab1e419d2ceb2
adam-gitlab <adam.threlfall@students.fhnw.ch>
2022-05-31 21:47:19 +0100

    added screenshot of join to screenshot folder

--- Ass2/src/full_join.sql
+++ Ass2/src/full_join.sql
@@ -5,4 +5,4 @@ full outer join car_index ci on oc.car_index_id = ci.car_id
 full outer join customer c on oc.customer_id = c.customer_id
 full outer join city cy on c.zip_code = cy.city_zip
 full outer join insurance_package ip on oc.package_id = ip.package_id
-full outer join insurance_company ic on ip.company_id = ic.company_id
+full outer join insurance_company ic on ip.company_id = ic.company_id;
