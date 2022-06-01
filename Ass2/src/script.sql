5a6beeda4b2866ce4f1867777a9951848d9662ef
Adam <adam.threlfall@students.fhnw.ch>
2022-06-01 17:25:36 +0200

    moved all sql to one file and renamed it to script.sql

--- Ass2/src/script.sql
+++ Ass2/src/script.sql
@@ -232,6 +232,15 @@ INSERT INTO owned_cars VALUES
 INSERT INTO owned_cars VALUES
 (10,1524,2,41,1,1001);
 
+---- Join
+select *
+from owned_cars oc
+full outer join car_colour cc on oc.car_colour_id = cc.colour_id
+full outer join car_index ci on oc.car_index_id = ci.car_id
+full outer join customer c on oc.customer_id = c.customer_id
+full outer join city cy on c.zip_code = cy.city_zip
+full outer join insurance_package ip on oc.package_id = ip.package_id
+full outer join insurance_company ic on ip.company_id = ic.company_id;
 
 ----Stored Procedure
 --Prints company and available packages when the company id is provided
